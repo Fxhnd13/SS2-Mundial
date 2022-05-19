261b78c76a1b6fa1b5eacad08937c23544f3ac44
Soberanis <jcsru13@gmail.com>
2022-05-18 23:07:00 -0600

    Add: limpieza final, modificaciones a db

---
+++ Scripts/DeleteFromDb.sql
@@ -0,0 +1,19 @@
+USE [Mundial]
+GO
+
+DELETE FROM [dbo].[Fact_Gol]
+GO
+DELETE FROM [dbo].[Dim_Jugador]
+GO
+DELETE FROM [dbo].[Dim_Partido]
+GO
+DELETE FROM [dbo].[Dim_Equipo]
+GO
+DELETE FROM [dbo].[Dim_Mundial]
+GO
+DELETE FROM [dbo].[Dim_Seleccion]
+GO
+DELETE FROM [dbo].[Dim_Tiempo_Gol]
+GO
+DELETE FROM [dbo].[Dim_Tiempo_Partido]
+GO
